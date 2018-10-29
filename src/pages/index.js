ced90fc6fdfadfb88962c9699e460b0b7cb1c032
kyranmccann <kyran.mccann@gmail.com>
2018-10-28 22:18:30 -0700

    fixing typo

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,7 +8,7 @@ const IndexPage = () => (
   <Layout>
     <h1>Hello, World!</h1>
     <p>Everybody knows the FCC JAMstack Hackathon is gonna be the coolest thing since sliced bread.</p>
-    <p>I'm Kyran. I'm less than a year into coding, but I'm in my third month at Lambda School. I'm a career switcher, but I feel like I've finally found my professional home in coding. </p>
+    <p>I'm Kyran. I'm less than a year into coding, but I'm in my third month at Lambda School. I'm a career switcher, and I feel like I've finally found my professional home in coding. </p>
     <div style={{ maxWidth: '300px', marginBottom: '1.45rem' }}>
       <Image />
     </div>
